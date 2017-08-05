8879b805e4247fa27eb2492c7ce4c136e0cc2c42
Scott Spalding <spaldingscott@gmail.com>
2017-08-04 20:33:43 -0700

    Initial commit

---
+++ Source/SeattleGameJam2017/SeattleGameJam2017.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SeattleGameJam2017.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SeattleGameJam2017, "SeattleGameJam2017" );
---
+++ Source/SeattleGameJam2017/SeattleGameJam2017GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SeattleGameJam2017GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEATTLEGAMEJAM2017_API ASeattleGameJam2017GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
